901d07ab001eeeeaeb913ebf06115086151e204e
Alesya103 <alesya.afanaseva27@gmail.com>
2020-10-05 15:41:36 +0300

    fix folder tree

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('styles/scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('styles/css'))
 });
